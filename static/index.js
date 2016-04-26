caaba0300beeae285392b4976c7c5ce9a9688b04
jinjinyun <jinyun.jin@gmail.com>
2016-04-26 15:48:20 +0800

    update apis

--- static/index.js
+++ static/index.js
@@ -1,16 +1,7 @@
 document.getElementById('refresh').onclick = function(){location.reload();}
 
-/**
- *  以下内容多摘自官方demo
- *
-**/
-wx.config({
-    debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
-    appId: appId, // 必填，公众号的唯一标识
-    timestamp: timestamp, // 必填，生成签名的时间戳
-    nonceStr: nonceStr, // 必填，生成签名的随机串
-    signature: signature,// 必填，签名，见附录1
-    jsApiList: ['checkJsApi',
+
+var jsApiList = ['checkJsApi',
         'onMenuShareTimeline',
         'onMenuShareAppMessage',
         'onMenuShareQQ',
@@ -43,19 +34,27 @@ wx.config({
         'openProductSpecificView',
         'addCard',
         'chooseCard',
-        'openCard'] // 必填，需要使用的JS接口列表，
+        'openCard']
+/**
+ *  以下内容多摘自官方demo
+ *
+**/
+wx.config({
+    debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+    appId: appId, // 必填，公众号的唯一标识
+    timestamp: timestamp, // 必填，生成签名的时间戳
+    nonceStr: nonceStr, // 必填，生成签名的随机串
+    signature: signature,// 必填，签名，见附录1
+    jsApiList: jsApiList // 必填，需要使用的JS接口列表，
 });
 
 wx.ready(function(){
  // 1 判断当前版本是否支持指定 JS 接口，支持批量判断
   document.querySelector('#checkJsApi').onclick = function () {
     wx.checkJsApi({
-      jsApiList: [
-        'getNetworkType',
-        'previewImage'
-      ],
+      jsApiList: jsApiList,
       success: function (res) {
-        alert(JSON.stringify(res));
+        alert('api supports:\n',JSON.stringify(res));
       }
     });
   };
